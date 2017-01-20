574c4dd6293a21954e93984f582d0f1412c9298c
gary lu <lv.g.gary@gmail.com>
2017-01-20 10:07:19 -0800

    add files

---
+++ smarthome/src/main/java/com/glu/smarthome/SpringApplication.java
@@ -0,0 +1,13 @@
+package com.glu.smarthome;
+
+/**
+ * Hello world!
+ *
+ */
+public class SpringApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
