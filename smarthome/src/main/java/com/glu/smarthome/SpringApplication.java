b023ebdb4138ef707f605efb25e5f658677184d1
gary lu <lv.g.gary@gmail.com>
2017-01-20 11:18:08 -0800

    update

--- smarthome/src/main/java/com/glu/smarthome/SpringApplication.java
+++ smarthome/src/main/java/com/glu/smarthome/SpringApplication.java
@@ -8,6 +8,6 @@ public class SpringApplication
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println(String.format("%s %s", args[0], args[1]));
     }
 }
